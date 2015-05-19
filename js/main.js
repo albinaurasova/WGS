7436222ed59f765c913b5f2fa34c058970dd9b3b
Михаил <beatbox787@gmail.com>
2015-05-20 05:31:34 +0600

    no message

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){	
 
+    function resize(){
+       if( typeof( window.innerWidth ) == 'number' ) {
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || 
+        document.documentElement.clientHeight ) ) {
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+        myWidth = ( myWidth < 1630 )?1630:myWidth;
+        myHeight = myWidth/16*9;
+        $(".b-video").css({
+            "width" : myWidth,
+            "height" : myHeight
+        });
+    }
+    $(window).resize(resize);
+    resize();
+
     var Cars = {
     "alfa_romeo" : {
         "156" : ["1.6i (120)","1.8i (144)"],
